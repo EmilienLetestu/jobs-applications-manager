585b7aa609d1204a2a4d2aeb43835234e1d959e8
eletestu@gmail.com <emilien.letestu@crm-centric.com>
2019-06-29 21:46:53 +0200

    wip : set up action/reponder and logic for appointment

---
+++ src/Action/CreateAppointmentAction.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emilien
+ * Date: 29/06/2019
+ * Time: 21:03
+ */
+
+namespace App\Action;
+
+use App\Responder\CreateAppointmentResponder as Responder;
+use App\Handler\Interfaces\CreateAppointmentHandlerInterface as FormHandlerInterface;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class CreateAppointmentAction
+{
+    /**
+     * @var FormFactoryInterface
+     */
+    private $formFactory;
+
+    /**
+     * @var FormHandlerInterface
+     */
+    private $handler;
+
+    /**
+     * @var UrlGeneratorInterface
+     */
+    private $urlGenerator;
+
+    /**
+     * @var SessionInterface
+     */
+    private $session;
+
+    /**
+     * CreateAppointmentAction constructor.
+     * @param FormFactoryInterface $formFactory
+     * @param FormHandlerInterface $handler
+     * @param UrlGeneratorInterface $urlGenerator
+     * @param SessionInterface $session
+     */
+    public function __construct(
+        FormFactoryInterface  $formFactory,
+        FormHandlerInterface  $handler,
+        UrlGeneratorInterface $urlGenerator,
+        SessionInterface      $session
+
+    )
+    {
+        $this->formFactory  = $formFactory;
+        $this->handler      = $handler;
+        $this->urlGenerator = $urlGenerator;
+        $this->session      = $session;
+    }
+
+    public function __invoke(Request $request, Responder $responder): Response
+    {
+        return $responder();
+    }
+}
---
+++ src/Responder/CreateAppointmentResponder.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emilien
+ * Date: 29/06/2019
+ * Time: 21:04
+ */
+
+namespace App\Responder;
+
+
+use Symfony\Component\Form\FormView;
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+
+class CreateAppointmentResponder
+{
+    /**
+     * @var Environment
+     */
+    private $twig;
+
+    /**
+     * CreateAppointmentResponder constructor.
+     * @param Environment $twig
+     */
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * @param FormView $form
+     * @return Response
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function __invoke(FormView $form): Response
+    {
+        return new Response(
+            $this->twig->render('createAppointment.html.twig',[
+                'form' => $form
+            ])
+        );
+    }
+}
