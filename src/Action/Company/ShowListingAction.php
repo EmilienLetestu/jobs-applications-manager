16c6588586af3e5082557a60bdcd0ecfd1692cad
eletestu@gmail.com <emilien.letestu@crm-centric.com>
2019-06-30 21:58:36 +0200

    fetch and display company listing

--- src/Action/Company/ShowListingAction.php
+++ src/Action/Company/ShowListingAction.php
@@ -8,8 +8,40 @@
 
 namespace App\Action\Company;
 
+use App\Repository\Interfaces\CompanyRepositoryInterface;
+use App\Responder\Company\ShowListingResponder as Responder;
+use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class ShowListingAction
+ * @package App\Action\Company
+ */
 class ShowListingAction
 {
+    /**
+     * @var
+     */
+    private $repository;
 
+    /**
+     * ShowListingAction constructor.
+     * @param CompanyRepositoryInterface $repository
+     */
+    public function __construct(CompanyRepositoryInterface $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * @Route("/company", name="company_listing", methods={"GET"})
+     * @param Responder $responder
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function __invoke(Responder $responder)
+    {
+       return$responder($this->repository->findAllCompanies());
+    }
 }
