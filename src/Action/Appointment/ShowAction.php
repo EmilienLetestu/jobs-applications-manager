0f311dee486badba7845a706ef5c0061e894a58b
eletestu@gmail.com <emilien.letestu@crm-centric.com>
2019-07-01 00:19:21 +0200

    fetch and display a given appointment

--- src/Action/Appointment/ShowAction.php
+++ src/Action/Appointment/ShowAction.php
@@ -9,7 +9,46 @@
 namespace App\Action\Appointment;
 
 
+use App\Repository\Interfaces\AppointmentRepositoryInterface;
+use App\Responder\Appointment\ShowResponder as Responder;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 class ShowAction
 {
+    /**
+     * @var AppointmentRepositoryInterface
+     */
+    private $repository;
 
+    /**
+     * ShowListingAction constructor.
+     * @param AppointmentRepositoryInterface $repository
+     */
+    public function __construct(AppointmentRepositoryInterface $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * @Route(
+     *     "/appointment/{id}",
+     *     name="show_appointment",
+     *     requirements={"id" = "\d+"},
+     *     methods={"GET"}
+     * )
+     * @param Responder $responder
+     * @param Request $request
+     * @return Response
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function __invoke(Responder $responder, Request $request): Response
+    {
+        return $responder(
+            $this->repository->findAppointmentWithId($request->get('id'))
+        );
+    }
 }
--- src/Responder/Appointment/ShowResponder.php
+++ src/Responder/Appointment/ShowResponder.php
@@ -9,7 +9,43 @@
 namespace App\Responder\Appointment;
 
 
+use App\Entity\Appointment;
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+
+/**
+ * Class ShowResponder
+ * @package App\Responder\Appointment
+ */
 class ShowResponder
 {
+    /**
+     * @var Environment
+     */
+    private $twig;
 
+    /**
+     * ShowResponder constructor.
+     * @param Environment $twig
+     */
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * @param Appointment $appointment
+     * @return Response
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function __invoke(Appointment $appointment): Response
+    {
+        return new Response(
+            $this->twig->render('appointment/show.html.twig',[
+                'appointment' => $appointment
+            ])
+        );
+    }
 }
