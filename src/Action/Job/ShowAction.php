f43e9e7c3d577edc0c3cd0fe33e81f79256b8504
eletestu@gmail.com <emilien.letestu@crm-centric.com>
2019-07-01 14:20:32 +0200

    validate Job\ShowActionTest

--- src/Action/Job/ShowAction.php
+++ src/Action/Job/ShowAction.php
@@ -9,7 +9,46 @@
 namespace App\Action\Job;
 
 
+use App\Repository\Interfaces\JobRepositoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+use App\Responder\Job\ShowResponder as Responder;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 class ShowAction
 {
+    /**
+     * @var JobRepositoryInterface
+     */
+    private $repository;
 
+    /**
+     * ShowAction constructor.
+     * @param JobRepositoryInterface $repository
+     */
+    public function __construct(JobRepositoryInterface $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * @Route(
+     *     "/job/{id}",
+     *     name="show_job",
+     *     requirements={"id" = "\d+"},
+     *     methods={"GET"}
+     * )
+     * @param Request $request
+     * @param Responder $responder
+     * @return Response
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function __invoke(Request $request, Responder $responder): Response
+    {
+        return $responder(
+            $this->repository->findJobWithId($request->get('id'))
+        );
+    }
 }
--- src/Responder/Job/ShowResponder.php
+++ src/Responder/Job/ShowResponder.php
@@ -8,18 +8,42 @@
 
 namespace App\Responder\Job;
 
+use App\Entity\Job;
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
 
+/**
+ * Class ShowResponder
+ * @package App\Responder\Job
+ */
 class ShowResponder
 {
+    /**
+     * @var
+     */
     private $twig;
 
-    public function __invoke()
+    /**
+     * @param Environment $twig
+     */
+    public function __construct(Environment $twig)
     {
-
+        $this->twig = $twig;
     }
 
-    public function __construct()
+    /**
+     * @param Job $job
+     * @return Response
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function __invoke(Job $job): Response
     {
-        
+        return new Response(
+            $this->twig->render('job/show.html.twig',[
+                'job' => $job
+            ])
+        );
     }
 }
