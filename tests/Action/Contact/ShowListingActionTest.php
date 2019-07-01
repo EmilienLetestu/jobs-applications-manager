44bdab5f82c99173216ffd0062017d204a892444
eletestu@gmail.com <emilien.letestu@crm-centric.com>
2019-07-01 16:24:25 +0200

    wrote test for Contact/ShowListingAction test

---
+++ tests/Action/Contact/ShowListingActionTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emilien
+ * Date: 01/07/2019
+ * Time: 16:23
+ */
+
+namespace App\Tests\Action\Contact;
+
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ShowListingActionTest extends WebTestCase
+{
+    public function testShowListingAction()
+    {
+        
+    }
+}
