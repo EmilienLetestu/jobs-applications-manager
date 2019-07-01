6cff6461a8efa0fae70865d3dbe5568b5b2d9ed1
eletestu@gmail.com <emilien.letestu@crm-centric.com>
2019-07-01 15:13:15 +0200

    added logic to fetch and display all applications in order to validate test

--- src/Action/Application/ShowListingAction.php
+++ src/Action/Application/ShowListingAction.php
@@ -9,7 +9,43 @@
 namespace App\Action\Application;
 
 
+use App\Repository\Interfaces\ApplicationRepositoryInterface;
+use App\Responder\Application\ShowListingResponder as Responder;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 class ShowListingAction
 {
+    /**
+     * @var ApplicationRepositoryInterface
+     */
+    private $repository;
 
+    /**
+     * ShowListingAction constructor.
+     * @param ApplicationRepositoryInterface $repository
+     */
+    public function __construct(ApplicationRepositoryInterface $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * @Route(
+     *     "/application",
+     *     name="application_listing",
+     *     methods={"GET"}
+     * )
+     * @param Responder $responder
+     * @return Response
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function __invoke(Responder $responder): Response
+    {
+        return $responder(
+            $this->repository->findAllApplications()
+        );
+    }
 }
--- src/Responder/Application/ShowListingResponder.php
+++ src/Responder/Application/ShowListingResponder.php
@@ -9,7 +9,38 @@
 namespace App\Responder\Application;
 
 
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+
 class ShowListingResponder
 {
-    
+    /**
+     * @var Environment
+     */
+    private $twig;
+
+    /**
+     * ShowListingResponder constructor.
+     * @param Environment $twig
+     */
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * @param array $applications
+     * @return Response
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function __invoke(array $applications): Response
+    {
+        return new Response(
+            $this->twig->render('application/show_listing.html.twig',[
+                'applications' => $applications
+            ])
+        );
+    }
 }
