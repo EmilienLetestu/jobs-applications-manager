970a42de7888da698c824edb152e4b7851fbaf85
eletestu@gmail.com <emilien.letestu@crm-centric.com>
2019-07-01 15:01:46 +0200

    added basic action, responder and template to list all applications

---
+++ src/Action/Application/ShowListingAction.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emilien
+ * Date: 01/07/2019
+ * Time: 14:58
+ */
+
+namespace App\Action\Application;
+
+
+class ShowListingAction
+{
+
+}
---
+++ src/Responder/Application/ShowListingResponder.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emilien
+ * Date: 01/07/2019
+ * Time: 14:59
+ */
+
+namespace App\Responder\Application;
+
+
+class ShowListingResponder
+{
+    
+}
